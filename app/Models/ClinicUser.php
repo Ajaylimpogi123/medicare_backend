13ca6cec6527e3926ea982cfaa12c1dd2dea718d
CraveTech <cravetech2012@gmail.com>
2026-07-27 21:13:16 +0800

    add seederlast

--- app/Models/ClinicUser.php
+++ app/Models/ClinicUser.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ClinicUser extends Model
 {
+    protected $table = 'clinic_user';
+
     protected $fillable = [
         'clinic_id',
         'user_id',
